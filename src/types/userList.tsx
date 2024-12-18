a89f25b1d99b5f9818b47b01f667c411ceaa885e
Aleena <toaleenavshaji@gmail.com>
2024-12-18 14:36:36 +0530

    types are maded in separate folder

--- src/types/userList.tsx
+++ src/types/userList.tsx
@@ -2,3 +2,13 @@ export interface User {
   username: string;
   password: string;
 }
+
+export interface UserFormProps {
+  onSubmit: (user: User) => void;
+  heading: string;
+  error?: string;
+  buttonText: string;
+  linkText?: string;
+  linkUrl?: string;
+  signText?: string;
+}
