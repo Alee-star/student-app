ede4168f1d3292c8110c434ef5301ae0d9fa80f2
Aleena <toaleenavshaji@gmail.com>
2024-12-27 17:22:45 +0530

    axios instance is added to loginpage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import axiosPage from "../axios";
 import UserForm from "../components/UserForm";
 import { User } from "../types/userList";
 import { AppRoutes } from "../routes/path";
@@ -13,7 +13,7 @@ const Login = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/users");
+        const response = await axiosPage.get("users");
         setUsers(response.data);
       } catch (err: any) {
         console.error("Error in fetching", err.message);
--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,14 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "../api";
+import { Student } from "../types/userList";
 
 const StudentsPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [students, setStudents] = useState<Student[]>([]);
+  const [filteredStudents, setFilteredStudents] = useState<Student[]>([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const response = await api.get("/data.json");
+        const classes = response.data.classes;
+        const allStudents = classes.flatMap((cls: any) => cls.students);
+        setStudents(allStudents);
+        setFilteredStudents([]);
+      } catch (error) {
+        console.error("Error fetching students data:", error);
+      }
+    };
+    fetchStudents();
+  }, []);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleSearch = () => {
+    const filtered = students.filter((student) =>
+      student.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredStudents(filtered);
+    setHasSearched(true);
+  };
+
   return (
-    <div className="bg-students bg-cover h-screen flex flex-col p-6">
+    <div className="bg-students bg-cover h-screen flex flex-col p-6 items-center">
       <div className="w-full p-6 rounded-lg flex items-center space-x-3">
         <input
           type="text"
@@ -17,9 +45,29 @@ const StudentsPage = () => {
           onChange={handleSearchChange}
           className="w-full p-3 border border-gray-300 rounded-lg outline-none"
         />
-        <button className="p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        <button
+          onClick={handleSearch}
+          className="p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        >
           Search
         </button>
+      </div>
+      <div className="bg-white z-10 shadow-md rounded-lg p-6 w-80 text-center">
+        <h1 className="text-xl font-bold">Students</h1>
+        {hasSearched && filteredStudents.length === 0 ? (
+          <p>No Students Found</p>
+        ) : (
+          <ul>
+            {filteredStudents.map((student) => (
+              <li
+                key={student.id}
+                className="mb-2 text-lg flex justify-between border-b pb-2"
+              >
+                <span className="capitalize">{student.name}</span>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
