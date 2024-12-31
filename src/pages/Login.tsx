a3c2a34619786558e59077bf7e78af74ec1ba8de
Aleena <toaleenavshaji@gmail.com>
2024-12-31 15:14:55 +0530

    data.json is maded db.json

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import UserForm from "../components/UserForm";
 import { User } from "../types/userList";
 import { AppRoutes } from "../routes/path";
-import axiosPage from "../axios";
+import api from "../api";
 
 const Login = () => {
   const [error, setError] = useState("");
@@ -13,7 +13,7 @@ const Login = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axiosPage.get("users");
+        const response = await api.get("users");
         setUsers(response.data);
       } catch (err: any) {
         console.error("Error in fetching", err.message);
